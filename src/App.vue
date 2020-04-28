3fc804e1afb3c86bed910a86c7576e784f034612
123 <583820225@qq.com>
2020-04-28 16:22:54 +0800

    npm run build报错

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+   hhhhhhhhhhhhhhhhhnpm
+  </div>
+</template>
+
+<script>
+// import header from './components/header/header.vue'
+// export default { // 注册header
+//   components: {
+//     header
+//   }
+// }
+</script>
+
+<style>
+</style>
